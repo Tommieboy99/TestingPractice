bde79add56eb96b95ab0d24e5e71efe19663b116
Tommieboy99 <tomkoek99@hotmail.com>
2025-11-14 16:46:08 +0100

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  // 1. Source files (browser)
+  {
+    ignores: ['dist/**', 'node_modules/**', 'package-lock.json'],
+
+    files: ['src/**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+  },
+
+  // 2. Webpack config files (node)
+  {
+    files: ['webpack.*.js'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+  },
+
+  {
+    overrides: [
+      {
+        files: ['tests/**/*'],
+        plugins: ['jest'],
+        env: {
+          'jest/globals': true,
+        },
+      },
+    ],
+  },
+
+  // 3. Prettier plugin
+  eslintPluginPrettierRecommended,
+]);
